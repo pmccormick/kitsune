4b20beaf3401ccf398ad8bc4d65987553603c1c7
William Moses <gh@wsmoses.com>
2017-10-07 23:41:30 -0400

    OpenMP Backend for Tapir (#18)
    * Initial move towards multi backend
    
    * [WIP] OpenMP backend
    
    * Finish omp working v1
    
    * Add openmp
    
    * Working OpenMP
    
    * Remove simplifycfg option
    
    * Add fast omp option

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -179,11 +179,11 @@ extern cl::opt<bool> EnableKnowledgeRetention;
 } // namespace llvm
 
 PassManagerBuilder::PassManagerBuilder() {
-    InstrumentCilk = false;
+    tapirTarget = nullptr;
+    DisableTapirOpts = false;
+    Rhino = false;
     OptLevel = 2;
     SizeLevel = 0;
-    ParallelLevel = 0;
-    Rhino = false;
     LibraryInfo = nullptr;
     Inliner = nullptr;
     DisableUnrollLoops = false;
@@ -603,10 +603,10 @@ void PassManagerBuilder::populateModulePassManager(
       Inliner = nullptr;
     }
 
-    if (ParallelLevel > 0) {
+    if (tapirTarget) {
       MPM.add(createInferFunctionAttrsLegacyPass());
       // MPM.add(createUnifyFunctionExitNodesPass());
-      MPM.add(createLowerTapirToCilkPass(ParallelLevel == 2, InstrumentCilk));
+      MPM.add(createLowerTapirToTargetPass(tapirTarget));
       // The lowering pass may leave cruft around.  Clean it up.
       MPM.add(createCFGSimplificationPass());
       MPM.add(createInferFunctionAttrsLegacyPass());
@@ -635,14 +635,22 @@ void PassManagerBuilder::populateModulePassManager(
   addInitialAliasAnalysisPasses(MPM);
 
   bool RerunAfterTapirLowering = false;
-  bool TapirHasBeenLowered = (ParallelLevel == 0);
-  if (ParallelLevel == 3) // -fdetach
-    MPM.add(createLowerTapirToCilkPass(false, InstrumentCilk));
+  bool TapirHasBeenLowered = (tapirTarget == nullptr);
+
+  if (tapirTarget && DisableTapirOpts) {// -fdetach
+    MPM.add(createLowerTapirToTargetPass(tapirTarget));
+    TapirHasBeenLowered = true;
+  }
 
   do {
     RerunAfterTapirLowering =
+<<<<<<< HEAD
        !TapirHasBeenLowered && (ParallelLevel > 0) && !PrepareForThinLTO;
 
+=======
+       !TapirHasBeenLowered && tapirTarget && !PrepareForThinLTO;
+
+>>>>>>> e2b3907f3d71 (OpenMP Backend for Tapir (#18))
   // Infer attributes about declarations if possible.
   MPM.add(createInferFunctionAttrsLegacyPass());
 
@@ -828,7 +836,7 @@ void PassManagerBuilder::populateModulePassManager(
   MPM.add(createCFGSimplificationPass(
       SimplifyCFGOptions().convertSwitchRangeToICmp(true)));
 
-  if (RerunAfterTapirLowering || (ParallelLevel == 0))
+  if (RerunAfterTapirLowering || (tapirTarget == nullptr))
     // Add passes to run just before Tapir lowering.
     addExtensionsToPM(EP_TapirLate, MPM);
 
@@ -841,7 +849,7 @@ void PassManagerBuilder::populateModulePassManager(
     // relies on the rotated form.  Disable header duplication at -Oz.
     MPM.add(createLoopRotatePass(SizeLevel == 2 ? 0 : -1));
 
-    MPM.add(createLoopSpawningPass());
+    MPM.add(createLoopSpawningPass(tapirTarget));
 
     // The LoopSpawning pass may leave cruft around.  Clean it up.
     MPM.add(createLoopDeletionPass());
@@ -849,14 +857,14 @@ void PassManagerBuilder::populateModulePassManager(
     addInstructionCombiningPass(MPM);
     addExtensionsToPM(EP_Peephole, MPM);
 
-    // Now lower Tapir to Cilk runtime calls.
+    // Now lower Tapir to Target runtime calls.
     //
     // TODO: Make this sequence of passes check the library info for the Cilk
     // RTS.
 
     MPM.add(createInferFunctionAttrsLegacyPass());
     // MPM.add(createUnifyFunctionExitNodesPass());
-    MPM.add(createLowerTapirToCilkPass(ParallelLevel == 2, InstrumentCilk));
+    MPM.add(createLowerTapirToTargetPass(tapirTarget));
     // The lowering pass may leave cruft around.  Clean it up.
     MPM.add(createCFGSimplificationPass());
     MPM.add(createInferFunctionAttrsLegacyPass());
@@ -872,8 +880,6 @@ void PassManagerBuilder::populateModulePassManager(
   MPM.add(createAnnotationRemarksLegacyPass());
 }
 
-<<<<<<< HEAD
-=======
 // void PassManagerBuilder::populateModulePassManager(legacy::PassManagerBase& MPM) {
 //   if (ParallelLevel != 0) {
 //     switch (ParallelLevel) {
