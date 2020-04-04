cf4eea9b281830f22e6e9af451714ca72cdd9c9c
TB Schardl <neboat@mit.edu>
2020-04-04 23:22:37 +0000

    [CGCilk] Simplify the code for creating a sync region with an implicit sync.

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -1276,8 +1276,9 @@ public:
   /// The current sync region.
   SyncRegion *CurSyncRegion = nullptr;
 
-  void PushSyncRegion() {
+  SyncRegion *PushSyncRegion() {
     CurSyncRegion = new SyncRegion(*this);
+    return CurSyncRegion;
   }
 
   llvm::Instruction *EmitSyncRegionStart();
