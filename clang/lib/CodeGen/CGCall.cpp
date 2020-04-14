bd66b258114dcf8c5d26af5c7fc75feb117b270b
TB Schardl <neboat@mit.edu>
2020-04-14 12:38:31 +0000

    [CodeGen] Emit implicit syncs before implicit destructors and other cleanups.

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2717,11 +2717,6 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
     }
   }
 
-  // For Cilk functions, ensure that a sync is implicitly executed before this
-  // function returns.
-  if (getLangOpts().Cilk)
-    EHStack.pushCleanup<ImplicitSyncCleanup>(NormalCleanup);
-
   // FIXME: We no longer need the types from FunctionArgList; lift up and
   // simplify.
 
@@ -3528,8 +3523,6 @@ void CodeGenFunction::EmitFunctionEpilog(const CGFunctionInfo &FI,
     Builder.CreateUnreachable();
     return;
   }
-
-  PopSyncRegion();
 
   // Functions with no result always return void.
   if (!ReturnValue.isValid()) {
