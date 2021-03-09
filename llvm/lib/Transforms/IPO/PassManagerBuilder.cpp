83f87300180042f0094bba843f45a1ad3cd5d4e0
TB Schardl <neboat@mit.edu>
2021-03-09 19:24:24 +0000

    Bug fixes for rebase onto LLVM 11.1.0

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -844,7 +844,7 @@ void PassManagerBuilder::populateModulePassManager(
     MPM.add(createEarlyCSEPass());
     MPM.add(createJumpThreadingPass());         // Thread jumps
     MPM.add(createCorrelatedValuePropagationPass());
-    addInstructionCombiningPass(MPM);
+    MPM.add(createInstructionCombiningPass());
   }
 
   addExtensionsToPM(EP_VectorizerStart, MPM);
@@ -957,7 +957,7 @@ void PassManagerBuilder::populateModulePassManager(
 
     MPM.add(createDeadArgEliminationPass()); // Dead argument elimination
 
-    addInstructionCombiningPass(MPM); // Clean up after IPCP & DAE
+    MPM.add(createInstructionCombiningPass()); // Clean up after IPCP & DAE
     MPM.add(createCFGSimplificationPass()); // Clean up after IPCP & DAE
 
     if (MergeFunctions)
