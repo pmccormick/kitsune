OpenCilk/opencilk-project

[MemorySSA] Update memory SSA to recognize clobber queries within the same spindle and to use that spindle information for alias-analysis queries.

llvm/lib/Analysis/MemorySSA.cpp
@@ -99,6 +99,10 @@ static cl::opt<bool>
               cl::desc("Allow MemorySSA to assume the program is "
                        "data-race free."));
 
+static cl::opt<bool> RequireTI("require-taskinfo-memoryssa", cl::init(false),
+                               cl::Hidden,
+                               cl::desc("Require TaskInfo for MemorySSA."));
+
 namespace llvm {
 
 /// An assembly annotator class to print Memory SSA information in
@@ -324,7 +328,13 @@ instructionClobbersQuery(const MemoryDef *MD, const MemoryLocation &UseLoc,
   }
 
   if (UseCall) {
-    ModRefInfo I = AA.getModRefInfo(DefInst, UseCall);
+    bool SameSpindle = false;
+    if (TI && UseCall->isStrandPure())
+      if (TI->getSpindleFor(UseCall->getParent()) ==
+          TI->getSpindleFor(DefInst->getParent()))
+        SameSpindle = true;
+
+    ModRefInfo I = AA.getModRefInfo(DefInst, UseCall, SameSpindle);
     AR = isMustSet(I) ? MustAlias : MayAlias;
     return {isModOrRefSet(I), AR};
   }
@@ -2291,7 +2301,7 @@ MemorySSAAnalysis::Result MemorySSAAnalysis::run(Function &F,
                                                  FunctionAnalysisManager &AM) {
   auto &DT = AM.getResult<DominatorTreeAnalysis>(F);
   auto &AA = AM.getResult<AAManager>(F);
-  TaskInfo *TI = EnableDRF ? (&AM.getResult<TaskAnalysis>(F)) : nullptr;
+  TaskInfo *TI = &AM.getResult<TaskAnalysis>(F);
   return MemorySSAAnalysis::Result(
       std::make_unique<MemorySSA>(F, &AA, &DT, TI));
 }
@@ -2332,15 +2342,17 @@ void MemorySSAWrapperPass::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.setPreservesAll();
   AU.addRequiredTransitive<DominatorTreeWrapperPass>();
   AU.addRequiredTransitive<AAResultsWrapperPass>();
-  if (EnableDRF)
-    AU.addRequiredTransitive<TaskInfoWrapperPass>();
+  // TODO: Add TaskInfoWrapperPass to lib/Analysis/LoopPass.cpp to make this
+  // work?
+  if (RequireTI || EnableDRF)
+    AU.addRequired<TaskInfoWrapperPass>();
 }
 
 bool MemorySSAWrapperPass::runOnFunction(Function &F) {
   auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
   auto &AA = getAnalysis<AAResultsWrapperPass>().getAAResults();
-  TaskInfo *TI =
-    EnableDRF ? &getAnalysis<TaskInfoWrapperPass>().getTaskInfo() : nullptr;
+  auto *TIWP = getAnalysisIfAvailable<TaskInfoWrapperPass>();
+  TaskInfo *TI = TIWP ? &TIWP->getTaskInfo() : nullptr;
   MSSA.reset(new MemorySSA(F, &AA, &DT, TI));
   return false;
 }

pmccormick/kitsune

llvm/lib/Analysis/MemorySSA.cpp
@@ -102,6 +102,10 @@ static cl::opt<bool>
               cl::desc("Allow MemorySSA to assume the program is "
                        "data-race free."));
 
+static cl::opt<bool> RequireTI("require-taskinfo-memoryssa", cl::init(false),
+                               cl::Hidden,
+                               cl::desc("Require TaskInfo for MemorySSA."));
+
 const static char LiveOnEntryStr[] = "liveOnEntry";
 
 namespace {
@@ -355,7 +359,12 @@ instructionClobbersQuery(const MemoryDef *MD, const MemoryLocation &UseLoc,
   }
 
   if (auto *CB = dyn_cast_or_null<CallBase>(UseInst)) {
-    ModRefInfo I = AA.getModRefInfo(DefInst, CB);
+    bool SameSpindle = false;
+    if (TI && CB->isStrandPure() &&
+        (TI->getSpindleFor(CB->getParent()) ==
+         TI->getSpindleFor(DefInst->getParent())))
+      SameSpindle = true;
+    ModRefInfo I = AA.getModRefInfo(DefInst, CB, SameSpindle);
     AR = isMustSet(I) ? AliasResult::MustAlias : AliasResult::MayAlias;
     return {isModOrRefSet(I), AR};
   }
@@ -2426,7 +2435,7 @@ MemorySSAAnalysis::Result MemorySSAAnalysis::run(Function &F,
                                                  FunctionAnalysisManager &AM) {
   auto &DT = AM.getResult<DominatorTreeAnalysis>(F);
   auto &AA = AM.getResult<AAManager>(F);
-  TaskInfo *TI = EnableDRF ? (&AM.getResult<TaskAnalysis>(F)) : nullptr;
+  TaskInfo *TI = &AM.getResult<TaskAnalysis>(F);
   return MemorySSAAnalysis::Result(
       std::make_unique<MemorySSA>(F, &AA, &DT, TI));
 }
@@ -2484,15 +2493,17 @@ void MemorySSAWrapperPass::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.setPreservesAll();
   AU.addRequiredTransitive<DominatorTreeWrapperPass>();
   AU.addRequiredTransitive<AAResultsWrapperPass>();
-  if (EnableDRF)
-    AU.addRequiredTransitive<TaskInfoWrapperPass>();
+  // TODO: Add TaskInfoWrapperPass to lib/Analysis/LoopPass.cpp to make this
+  // work?
+  if (RequireTI || EnableDRF)
+    AU.addRequired<TaskInfoWrapperPass>();
 }
 
 bool MemorySSAWrapperPass::runOnFunction(Function &F) {
   auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
   auto &AA = getAnalysis<AAResultsWrapperPass>().getAAResults();
-  TaskInfo *TI =
-    EnableDRF ? &getAnalysis<TaskInfoWrapperPass>().getTaskInfo() : nullptr;
+  auto *TIWP = getAnalysisIfAvailable<TaskInfoWrapperPass>();
+  TaskInfo *TI = TIWP ? &TIWP->getTaskInfo() : nullptr;
   MSSA.reset(new MemorySSA(F, &AA, &DT, TI));
   return false;
 }