OpenCilk/opencilk-project

[PassBuilder][PassManagerBuilder] Run EarlyCSE to remove common subexpressions that emerge from SimplifyCFG's sinking optimization

llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -769,6 +769,8 @@ void PassManagerBuilder::populateModulePassManager(
   // before SLP vectorization.
   MPM.add(createTaskSimplifyPass());
   MPM.add(createCFGSimplificationPass(1, true, true, false, true));
+  // Rerun EarlyCSE for further cleanup after the sinking transformation.
+  MPM.add(createEarlyCSEPass(true /* Enable mem-ssa. */));
 
   if (SLPVectorize) {
     MPM.add(createSLPVectorizerPass()); // Vectorize parallel scalar chains.

pmccormick/kitsune

llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -559,6 +559,8 @@ void PassManagerBuilder::addVectorPasses(legacy::PassManagerBase &PM,
 
   if (IsFullLTO) {
     PM.add(createSCCPPass());                 // Propagate exposed constants
+    // Rerun EarlyCSE for further cleanup after the sinking transformation.
+    PM.add(createEarlyCSEPass(true /* Enable mem-ssa. */));
     PM.add(createInstructionCombiningPass()); // Clean up again
     PM.add(createBitTrackingDCEPass());
   }