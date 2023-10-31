d884674842afe05ece0a815318c7275c1b913c93
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2023-10-31 08:53:36 -0600

    Clean up some code cruft -- no need to duplicate else branch cases.

--- clang/lib/Sema/SemaStmtAttr.cpp
+++ clang/lib/Sema/SemaStmtAttr.cpp
@@ -498,22 +498,30 @@ static Attr *handleTapirTargetAttr(Sema &S, Stmt *St, const ParsedAttr &A,
     return nullptr;
   }
 
-  // We only support a limited range of statement classes.
-  // TODO: Add support for spawn and sync statements. 
+  // We only support a limited range of statements.  Make sure we are dealing 
+  // with one of them -- if not return an error.  
+  // 
+  // TODO: Should spawn and sync statements have special cases here? 
+  //
   if (St->getStmtClass() == Stmt::ForallStmtClass || 
       St->getStmtClass() == Stmt::CXXForallRangeStmtClass) {
     return ::new(S.Context)TapirTargetAttr(S.Context, A, tapirTK);
 
   } else if (Expr *E = dyn_cast<Expr>(St)) {
+
     if (S.getLangOpts().Kokkos) {
       // See if this is an attributed Kokkos parallel statement (if
-      // so, there is a CallExpr lurking here).  To find this CallExpr
-      // we need to "unwind" implicit details (including clean up).
-      // In essence we are simply "un-obfuscating" a bunch of C++
-      // constructs...  The most important detail here is that the
-      // attribute is really not attached to the Kokkos statement
-      // (even though it might appear to be from the programmer's
-      // perspective).
+      // so, there is a CallExpr lurking further down in the AST).  
+      // To find this CallExpr we need to work past implicit details 
+      // (including clean up). We need to "un-obfuscating" a bunch of 
+      // C++ mechanisms to really find what sort of expression we have.
+      // The most important detail here is that the attribute is really 
+      // not attached to the Kokkos statement (even though it might 
+      // appear to be so from the program syntax and thus the programmer's 
+      // perspective).  How lovely... 
+      //
+      // TODO: If we do more of this we should probably pull this code 
+      // block out into a function for easier use. 
       const Expr *ClarifiedE = E->IgnoreImplicit();
       const Expr *Last = nullptr;
       while(ClarifiedE != Last) {
@@ -521,17 +529,16 @@ static Attr *handleTapirTargetAttr(Sema &S, Stmt *St, const ParsedAttr &A,
         if (const auto *CE = dyn_cast<ExprWithCleanups>(ClarifiedE))
           ClarifiedE = CE->getSubExpr()->IgnoreImplicit();
       }
-      
+
+      // With all the extra "stuff" removed check to see if we have a
+      // CallExpr -- if so, see if we can recognize it as a Kokkos 
+      // construct... 
       if (const CallExpr *CE = dyn_cast<CallExpr>(ClarifiedE)) {
         const FunctionDecl *Fdecl = CE->getDirectCallee();
         std::string QName = Fdecl->getQualifiedNameAsString();
         if (QName == "Kokkos::parallel_for" ||
-            QName == "Kokkos::parallel_reduce") {
+            QName == "Kokkos::parallel_reduce")
           return ::new(S.Context)TapirTargetAttr(S.Context, A, tapirTK);
-        } else {
-          S.Diag(A.getLoc(), diag::warn_tapir_target_attr_bad_stmt_class);
-          return nullptr;
-        }
       }
     }
   }
